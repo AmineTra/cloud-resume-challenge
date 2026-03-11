d80bf8fff29dead6689a941e5638a7218cfe3184
Amine <traibi43@gmail.com>
2026-03-11 23:54:18 +0000

    Add backend logic, tests, and deployment pipeline

--- Backend/test_lambda.py
+++ Backend/test_lambda.py
@@ -1,23 +1,23 @@
 import unittest
-from unittest.mock import MagicMock 
+from unittest.mock import MagicMock
 import lambda_function
 
-class TestLambdaFunction(unittest.TestCase):
+class TestLambda(unittest.TestCase):
 
     def test_lambda_handler(self):
+        mock_table = MagicMock()
         
-        lambda_function.mock_table = MagicMock()
-        lambda_function.table.update_item.return_value ={
+        mock_table.update_item.return_value = {
             'Attributes': {'views': 100}
-            }
+        }
         
-        result = lambda_function.lambda_handler({}, {})
+        lambda_function.table = mock_table
+
+        
+        result = lambda_function.lambda_handler(None, None)
+
         self.assertEqual(result['statusCode'], 200)
-        
-        # Is the 'views' count inside the body?
         self.assertIn('views', result['body'])
-        
-        # Did we send the CORS headers? (Crucial check!)
         self.assertEqual(result['headers']['Access-Control-Allow-Origin'], '*')
 
 if __name__ == '__main__':
